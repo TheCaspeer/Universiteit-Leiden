41809bd9ed919b20018dc249b76ce32182978129
TheCaspeer <54185639+TheCaspeer@users.noreply.github.com>
2026-03-09 15:15:03 +0100

    Refactor: changed comments and code style

--- year1_semester2/Planetenstelsels/Computerpractica/pl_opdracht2.2.py
+++ year1_semester2/Planetenstelsels/Computerpractica/pl_opdracht2.2.py
@@ -1,14 +1,17 @@
 from astronomy import *
 import matplotlib.pyplot as plt
 import numpy as np
+import codecp2
+from scipy.optimize import minimize
 
-
+# Reading lightcurve data
 parent_dic = r'year2_semester2\Planetenstelsels\Computerpractica'
 file_loc1 = rf"{parent_dic}\WASP-203_lightcurve.dat"
 data1 = np.loadtxt (rf"{file_loc1}")
 phase =data1 [: ,0] # phase
 flux  =data1 [: ,1] # flux
 
+# Plotting lightcurve
 fig1, (ax1, ax2) = plt.subplots(1, 2, figsize=(14, 6))
 ax1.plot(phase, flux, 'o', markersize=1)
 ax1.set_title('Lightcurve of WASP-203 (raw)')
@@ -16,35 +19,32 @@ ax1.set_xlabel('Phase')
 ax1.set_ylabel('Flux')
 ax1.grid()
 
-
-import codecp2
-
+# Binning the data
 binsize = 1/(len(phase)/100) # 75 bins, iets te weinig atm lulw
 binned = codecp2.bincurve(phase ,flux , binsize)
 phase_bin = binned [0]
 flux_bin = binned [1]
 
+# Plotting binned lightcurve
 ax2.plot(phase_bin, flux_bin, 'o', markersize=1)
 ax2.set_title('Lightcurve of WASP-203 (binned)')
 ax2.set_xlabel('Phase')
 ax2.set_ylabel('Flux')
 ax2.grid()
 fig1.name = "Lightcurve of WASP-203"
-# plt.tight_layout()
-# plt.show()
 
-# the transit happens in flux (0.94,0.96)
+# The transit happens in flux (0.94,0.96), manually read from the plot
 y_transit = flux_bin[(flux_bin > 0.94) & (flux_bin < 0.96)]
 transit_flux = np.mean(y_transit) # average flux during transit 
 print(f"Transit flux: {transit_flux:.4f}% van de totale flux")
 
-
-# Radial velocity data and plot
+# Reading radial velocity data 
 file_loc2 = rf"{parent_dic}\WASP-203_RV.dat"
 data2 = np.loadtxt (rf"{file_loc2}")
 time = data2 [: ,0] # time [s]
 radial_velocity = data2 [: ,1] # radial velocity [km/s]
 
+# Plotting radial velocity data
 fig2, (ax3,ax4) =  plt.subplots(1, 2, figsize=(14, 6))
 ax3.scatter(time, radial_velocity, s=10)
 ax3.set_title('Radial Velocity of WASP-203')
@@ -53,15 +53,7 @@ ax3.set_ylabel('Radial Velocity (km/s)')
 ax3.grid()
 fig2.name = "Radial Velocity of WASP-203"
 
-
-# plt.tight_layout()
-# plt.show()
-
-# quit() # remove
-
-# Fitting radial velocity 
-from scipy.optimize import minimize
-
+# Fitting radial velocity (largely foreign code)
 gemeten_tijd = time/(3600.*24) # [s] -> [dagen]
 gemeten_radial_velocity = radial_velocity # [km/s]
 def fit_functie (variabelen , tijd , v_gemeten ):
@@ -87,24 +79,23 @@ x0 = np.array ([ K_gok , phi_gok , P_gok , K_0_gok ])
 #Hier wordt de functie gefit aan de data.
 x = minimize (fit_functie , x0 , args =( gemeten_tijd , gemeten_radial_velocity ),method = 'Powell').x
 
-#De resulterende parameters .
+#De resulterende parameters
 K = x[0]
 phi = x[1]
 P = x[2]
 K_0 = x[3]
 
-
 # Plotting fitted curve
 tijd_sample = np.linspace (gemeten_tijd.min(), gemeten_tijd.max(), 1000)
-
 def v_fitted(tijd):
    return K * np.sin (2 * np.pi * (tijd + phi) / P) + K_0
-
 ax4.plot(tijd_sample, v_fitted(tijd_sample), color='red', label='Fitted Curve')
 ax4.set_title('Fitted Radial Velocity Curve of WASP-203')
 ax4.set_xlabel('Time [days]')
 ax4.set_ylabel('Radial Velocity [km/s]')
 ax4.grid()
 
+
+# Showing all plots
 plt.tight_layout()
 plt.show()
