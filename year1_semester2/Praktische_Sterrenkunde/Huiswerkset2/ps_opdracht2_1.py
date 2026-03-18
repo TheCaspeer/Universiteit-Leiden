c79dde9cf5d239b76170333d34902152c29abe1a
Caspeer <54185639+TheCaspeer@users.noreply.github.com>
2026-03-18 21:05:06 +0100

    Feat: finished ps_opdracht2_1.py

--- year1_semester2/Praktische_Sterrenkunde/Huiswerkset2/ps_opdracht2_1.py
+++ year1_semester2/Praktische_Sterrenkunde/Huiswerkset2/ps_opdracht2_1.py
@@ -2,6 +2,7 @@ import astropy as ap
 from astropy import units as u
 from astropy import constants as const
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 # Known values
@@ -11,26 +12,65 @@ D = 16.8 * u.megaparsec # distance to M87
 sigma_D = 0.8 * u.megaparsec # uncertainty in distance to M87
 theta_g = (d / (2 * 27**0.5)).to('', equivalencies=u.dimensionless_angles())
 sigma_theta_g = (sigma_d / (2 * 27**0.5) ).to('', equivalencies=u.dimensionless_angles())
-sigma_theta_g = sigma_theta_g.to('', equivalencies=u.dimensionless_angles())
+
 # Analytical approach
 # the uncertainity in both c and G is regarded as 0 due to their high precision compared to the other values.
 analytical_sigma_M = ( (const.c**2 * D / const.G  * sigma_theta_g )**2 + (theta_g * const.c**2 / const.G * sigma_D )**2 )**0.5
 analytical_M = (theta_g * const.c**2 * D ) / const.G
 
 # Monte Carlo approach
-N = 500
-M_samples = []
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
+fig.suptitle('Monte Carlo Simulations of Black Hole Mass Estimation')
+N = 500 # number of Monte Carlo samples
 
-for i in range(N):
+def monte_carlo1_sample(d, sigma_d, D, sigma_D):
     d_sample = np.random.normal(d.value, sigma_d.value) * u.uas
     D_sample = np.random.normal(D.value, sigma_D.value) * u.Mpc
     theta_g_sample = (d_sample / (2 * 27**0.5)).to('', equivalencies=u.dimensionless_angles())
-    M_sample = (theta_g_sample * const.c**2 * D_sample / const.G)
-    M_samples.append(M_sample)
+    M_sample = (theta_g_sample * const.c**2 * D_sample / const.G) 
+    M_sample = M_sample.to(u.Msun)
+    return M_sample
 
-M_samples = u.Quantity(M_samples)
-mc_M = M_samples.mean()
-mc_sigma_M = M_samples.std()
+monte_carlo1_samples = u.Quantity([
+    monte_carlo1_sample(d, sigma_d, D, sigma_D)
+    for _ in range(N)
+    ]) # solar masses
+ax1.hist(monte_carlo1_samples, bins=15, alpha=0.7)
+ax1.set_title('Histogram of Monte Carlo Samples of M')
+mc1_M = monte_carlo1_samples.mean()
+mc1_sigma_M = monte_carlo1_samples.std()
 
-print(f"Analytical approach: M = {analytical_M.to(u.Msun):.2e} ± {analytical_sigma_M.to(u.Msun):.2e}")
-print(f"Monte Carlo approach: M = {mc_M.to(u.Msun):.2e} ± {mc_sigma_M.to(u.Msun):.2e}")
+def monte_carlo2_sample(d, sigma_d, D, sigma_D):
+    d_sample = np.random.normal(d.value, sigma_d.value) * u.uas
+    d_sample = d_sample/np.random.uniform(9.6,11.5) # removing the artificial's effect
+    D_sample = np.random.normal(D.value, sigma_D.value) * u.Mpc
+    theta_g_sample = (d_sample / (2 * 27**0.5)).to('', equivalencies=u.dimensionless_angles())
+    M_sample = (theta_g_sample * const.c**2 * D_sample / const.G) 
+    M_sample = M_sample.to(u.Msun)
+    return M_sample
+
+monte_carlo2_samples = u.Quantity([
+    monte_carlo2_sample(d, sigma_d, D, sigma_D)
+    for _ in range(N)
+    ]) # solar masses
+ax2.hist(monte_carlo2_samples, bins=15, alpha=0.7)
+ax2.set_title('Histogram of Monte Carlo Samples of M with added parameter alpha')
+mc2_M = monte_carlo2_samples.mean()
+mc2_sigma_M = monte_carlo2_samples.std()
+
+# Formatting plots
+for ax in [ax1, ax2]:
+    ax.ticklabel_format(style='sci')
+    ax.set_xscale('log')
+    ax.grid(True, 'both')
+    ax.set_xlabel('Mass (M_sun)') 
+    ax.set_ylabel('Frequency')
+
+print(f"The mass and error on the black hole are: M = {mc1_M:.2e} ± {mc1_sigma_M:.2e} (Monte Carlo approach without alpha)\n"
+      f"M = {mc2_M:.2e} ± {mc2_sigma_M:.2e} (Monte Carlo approach with alpha)\n" 
+      f"Analytical approach: M = {analytical_M.to(u.Msun):.2e} ± {analytical_sigma_M.to(u.Msun):.2e}")
+
+plt.show()
+
+#print(f"Analytical approach: M = {analytical_M.to(u.Msun):.2e} ± {analytical_sigma_M.to(u.Msun):.2e}")
+#print(f"Monte Carlo approach: M = {mc_M.to(u.Msun):.2e} ± {mc_sigma_M.to(u.Msun):.2e}")
